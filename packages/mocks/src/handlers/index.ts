97825c8a6fece45939f15e48a2afec29e565030e
Tobias havmøller Secher <tobias.secher@gmail.com>
2025-12-04 16:24:23 +0100

    Added wildcard to msw handler

--- packages/mocks/src/handlers/index.ts
+++ packages/mocks/src/handlers/index.ts
@@ -1,8 +1,10 @@
 import { http, HttpResponse } from 'msw'
 import user from './mocks/user.json'
+const BASE_URL = '*'
+
 export const handlers = [
   // User endpoints
-  http.get('https://api.github.com/users/tobias-secher', () => {
+  http.get(`${BASE_URL}/users/tobias-secher`, () => {
     return HttpResponse.json(user)
   }),
 ]

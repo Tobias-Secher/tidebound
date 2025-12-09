a134461b8952bae19b1c599af46cdb93fde7b41e
Tobias-Secher <tobias.secher@gmail.com>
2025-12-09 22:40:40 +0100

    Enhances component generation with Turbo
    Updates the Turbo generator configuration to improve
    component generation, including support for both UI
    components and templates with associated stories and tests.
    
    Adds a local settings file to allow bash access.

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -47,94 +47,63 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
       if (componentType === "ui" || componentType === "both") {
         actions.push({
           type: "add",
-          path: "{{ turbo.paths.root }}/packages/ui/src/{{ kebabCase name }}.tsx",
-          templateFile: "templates/component.hbs",
+          path: "{{ turbo.paths.root }}/packages/ui/src/{{ pascalCase name }}/{{ pascalCase name }}.tsx",
+          templateFile: "templates/react-component/ui/component.hbs",
+        });
+        actions.push({
+          type: "add",
+          path: "{{ turbo.paths.root }}/packages/ui/src/{{ pascalCase name }}/index.ts",
+          templateFile: "templates/react-component/shared/index.hbs",
         });
         actions.push({
           type: "add",
           path: "{{ turbo.paths.root }}/apps/docs/stories/ui/{{ pascalCase name }}.stories.tsx",
-          templateFile: "templates/component.stories.hbs",
+          templateFile: "templates/react-component/ui/component.stories.hbs",
         });
 
         if (data?.withTest) {
           actions.push({
             type: "add",
-            path: "{{ turbo.paths.root }}/packages/ui/src/{{ kebabCase name }}.test.tsx",
-            templateFile: "templates/component.test.hbs",
+            path: "{{ turbo.paths.root }}/packages/ui/src/{{ pascalCase name }}/{{ pascalCase name }}.test.tsx",
+            templateFile: "templates/react-component/ui/component.test.hbs",
           });
         }
       }
-
       // Template Component
       if (componentType === "template" || componentType === "both") {
         const templateFile =
           componentType === "both"
-            ? "templates/template-with-ui.hbs"
-            : "templates/template.hbs";
+            ? "templates/react-component/template/template-with-ui.hbs"
+            : "templates/react-component/template/template.hbs";
 
         actions.push({
           type: "add",
-          path: "{{ turbo.paths.root }}/packages/templates/src/{{ kebabCase name }}.tsx",
+          path: "{{ turbo.paths.root }}/packages/templates/src/{{ pascalCase name }}/{{ pascalCase name }}.tsx",
           templateFile: templateFile,
         });
         actions.push({
           type: "add",
+          path: "{{ turbo.paths.root }}/packages/templates/src/{{ pascalCase name }}/index.ts",
+          templateFile: "templates/react-component/shared/index.hbs",
+        });
+        actions.push({
+          type: "add",
           path: "{{ turbo.paths.root }}/apps/docs/stories/templates/{{ pascalCase name }}.stories.tsx",
-          templateFile: "templates/template.stories.hbs",
+          templateFile:
+            "templates/react-component/template/template.stories.hbs",
         });
 
         if (data?.withTest) {
           actions.push({
             type: "add",
-            path: "{{ turbo.paths.root }}/packages/templates/src/{{ kebabCase name }}.test.tsx",
-            templateFile: "templates/template.test.hbs",
+            path: "{{ turbo.paths.root }}/packages/templates/src/{{ pascalCase name }}/{{ pascalCase name }}.test.tsx",
+            templateFile:
+              "templates/react-component/template/template.test.hbs",
           });
         }
       }
 
       return actions;
     },
-  });
-
-  plop.setGenerator("example", {
-    description:
-      "An example Turborepo generator - creates a new file at the root of the project",
-    prompts: [
-      {
-        type: "input",
-        name: "file",
-        message: "What is the name of the new file to create?",
-        validate: (input: string) => {
-          if (input.includes(".")) {
-            return "file name cannot include an extension";
-          }
-          if (input.includes(" ")) {
-            return "file name cannot include spaces";
-          }
-          if (!input) {
-            return "file name is required";
-          }
-          return true;
-        },
-      },
-      {
-        type: "list",
-        name: "type",
-        message: "What type of file should be created?",
-        choices: [".md", ".txt"],
-      },
-      {
-        type: "input",
-        name: "title",
-        message: "What should be the title of the new file?",
-      },
-    ],
-    actions: [
-      {
-        type: "add",
-        path: "{{ turbo.paths.root }}/{{ dashCase file }}{{ type }}",
-        templateFile: "templates/turborepo-generators.hbs",
-      },
-    ],
   });
 }

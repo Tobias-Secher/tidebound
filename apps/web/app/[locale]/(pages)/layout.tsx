4be9f9f304bac81d7dabe70c84f9c11e58065dce
Tobias havmøller Secher <tobias.secher@gmail.com>
2025-12-19 22:23:05 +0100

    Added DA to the locale and fixed fonts

--- apps/web/app/[locale]/(pages)/layout.tsx
+++ apps/web/app/[locale]/(pages)/layout.tsx
@@ -17,11 +17,11 @@ type Props = {
 };
 
 const geistSans = localFont({
-  src: "../fonts/GeistVF.woff",
+  src: "../../fonts/GeistVF.woff",
   variable: "--font-geist-sans",
 });
 const geistMono = localFont({
-  src: "../fonts/GeistMonoVF.woff",
+  src: "../../fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
 });
 
--- packages/i18n/src/index.ts
+++ packages/i18n/src/index.ts
@@ -1,5 +1,5 @@
 // Supported locales - single source of truth
-export const locales = ["en", "de", "fr"] as const;
+export const locales = ["en", "de", "fr", 'da'] as const;
 export type Locale = (typeof locales)[number];
 export const defaultLocale: Locale = "en";
 

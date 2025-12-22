eefbc2279741cc9bcd48e3d734d4ec9942fec96c
Tobias-Secher <tobias.secher@gmail.com>
2025-12-22 23:41:50 +0100

    Fixed build issues

--- apps/docs/.storybook/main.ts
+++ apps/docs/.storybook/main.ts
@@ -1,13 +1,4 @@
 import type { StorybookConfig } from "@storybook/react-vite";
-import { join, dirname } from "path";
-
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
-function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, "package.json")));
-}
 
 const config: StorybookConfig = {
   stories: [
@@ -16,14 +7,14 @@ const config: StorybookConfig = {
   ],
 
   addons: [
-    getAbsolutePath("@storybook/addon-onboarding"),
-    getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@storybook/addon-interactions"),
-    getAbsolutePath("@storybook/addon-links"),
+    "@storybook/addon-onboarding",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-links",
   ],
 
   framework: {
-    name: getAbsolutePath("@storybook/react-vite"),
+    name: "@storybook/react-vite",
     options: {},
   },
 

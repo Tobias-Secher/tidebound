2618b923597ae57a6dd54aa761dcde307e47a022
Tobias-Secher <tobias.secher@gmail.com>
2026-01-08 22:30:07 +0100

    fix: update return type of getAbsolutePath function and resolve type error for Storybook module

--- apps/docs/.storybook/main.ts
+++ apps/docs/.storybook/main.ts
@@ -8,7 +8,7 @@ import { fileURLToPath } from 'url';
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value: string): any {
+function getAbsolutePath(value: string): string {
   return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
 }
 
@@ -46,7 +46,7 @@ const config: StorybookConfig = {
         test: /\.css$/,
         exclude: /\.module\.css$/,
         use: ['style-loader', 'css-loader'],
-      }
+      },
     );
 
     return config;
--- apps/docs/types.d.ts
+++ apps/docs/types.d.ts
@@ -2,3 +2,7 @@ declare module '*.module.css' {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
+
+// Storybook's type exports require 'node16'/'nodenext' moduleResolution,
+// but this project uses 'Bundler'. This declaration resolves the type error.
+declare module '@storybook/react-webpack5';
